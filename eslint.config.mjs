e5e385b486d1b6b82fc40581e5ba2089de442c27
DarkRisy <wintervlad597@gmail.com>
2025-04-17 08:22:52 +0500

    up3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,6 @@ const __dirname = dirname(__filename);
 // const eslintConfig = [
 //   ...compat.extends("next/core-web-vitals", "next/typescript"),
 // ];
-import { FlatCompat } from '@eslint/eslintrc'
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
